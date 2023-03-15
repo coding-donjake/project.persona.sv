5f81aeec4d8d1be8385685543b429d3bda144e42
coding-donjake <108913937+coding-donjake@users.noreply.github.com>
2023-03-15 17:02:13 +0800

    Update

--- c#/Version2/Version2/Form1.Designer.cs
+++ c#/Version2/Version2/Form1.Designer.cs
@@ -39,6 +39,8 @@ namespace Version2
             this.txtPersonName = new System.Windows.Forms.TextBox();
             this.btnSaveFace = new System.Windows.Forms.Button();
             this.labelFacesCount = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.picCamera)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picFaceDetection)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -125,17 +127,37 @@ namespace Version2
             // labelFacesCount
             // 
             this.labelFacesCount.AutoSize = true;
-            this.labelFacesCount.Location = new System.Drawing.Point(630, 420);
+            this.labelFacesCount.Location = new System.Drawing.Point(630, 408);
             this.labelFacesCount.Name = "labelFacesCount";
             this.labelFacesCount.Size = new System.Drawing.Size(48, 13);
             this.labelFacesCount.TabIndex = 9;
             this.labelFacesCount.Text = "Faces: 0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(630, 421);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "No data";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(630, 434);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "No data";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(884, 476);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.labelFacesCount);
             this.Controls.Add(this.btnSaveFace);
             this.Controls.Add(this.txtPersonName);
@@ -171,6 +193,8 @@ namespace Version2
         private System.Windows.Forms.TextBox txtPersonName;
         private System.Windows.Forms.Button btnSaveFace;
         private System.Windows.Forms.Label labelFacesCount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- c#/Version2/Version2/Form1.cs
+++ c#/Version2/Version2/Form1.cs
@@ -28,10 +28,50 @@ namespace Version2
 
         // cascade file for face detection
         CascadeClassifier faceCasacdeClassifier = new CascadeClassifier(@"haarcascade_frontalface_default.xml");
+
+        // for face recognition
+        EigenFaceRecognizer recognizer;
+        List<Image<Gray, Byte>> TrainedFaces = new List<Image<Gray, byte>>();
+        List<int> PersonsLabes = new List<int>();
+        List<string> PersonsNames = new List<string>();
         #endregion
         public Form1()
         {
             InitializeComponent();
+            loadSavedFaceFiles();
+        }
+
+        private void loadSavedFaceFiles()
+        {
+            int ImagesCount = 0;
+            TrainedFaces.Clear();
+            PersonsLabes.Clear();
+            PersonsNames.Clear();
+            try
+            {
+                string path = Directory.GetCurrentDirectory() + @"\SavedFaces";
+                string[] files = Directory.GetFiles(path, "*.jpg", SearchOption.AllDirectories);
+                foreach (var file in files)
+                {
+                    Image<Gray, byte> trainedImage = new Image<Gray, byte>(file).Resize(200, 200, Inter.Cubic);
+                    CvInvoke.EqualizeHist(trainedImage, trainedImage);
+                    TrainedFaces.Add(trainedImage);
+                    PersonsLabes.Add(ImagesCount);
+                    PersonsNames.Add(file.Split('\\').Last().Split('_')[0]);
+                    ImagesCount++;
+                    Debug.WriteLine(ImagesCount + ". " + file);
+                }
+
+                if (TrainedFaces.Count() > 0)
+                {
+                    recognizer = new EigenFaceRecognizer(ImagesCount, double.PositiveInfinity);
+                    recognizer.Train(TrainedFaces.ToArray(), PersonsLabes.ToArray());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in Train Images: " + ex.Message);
+            }
         }
 
         private void processFrame(object sender, EventArgs e)
@@ -70,6 +110,33 @@ namespace Version2
                         detectedFace.ROI = face;
                         pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
                         pictureBox1.Image = detectedFace.Bitmap;
+
+                        // recognize face
+                        if (PersonsNames.Count > 0)
+                        {
+                            Image<Gray, Byte> grayFaceResult = detectedFace.Convert<Gray, Byte>().Resize(200, 200, Inter.Cubic);
+                            CvInvoke.EqualizeHist(grayFaceResult, grayFaceResult);
+                            var result = recognizer.Predict(grayFaceResult);
+                            pictureBox2.Image = grayFaceResult.Bitmap;
+                            pictureBox3.Image = TrainedFaces[result.Label].Bitmap;
+                            label1.Text = result.Label + ". " + result.Distance;
+
+                            //Here results found known faces
+                            if (result.Label != -1 && result.Distance < 1000)
+                            {
+                                CvInvoke.PutText(currentFrame, PersonsNames[result.Label], new Point(face.X - 2, face.Y - 2),
+                                    FontFace.HersheyComplex, 1.0, new Bgr(Color.Orange).MCvScalar);
+                                CvInvoke.Rectangle(currentFrame, face, new Bgr(Color.Green).MCvScalar, 2);
+                            }
+
+                            //here results did not found any know faces
+                            else
+                            {
+                                CvInvoke.PutText(currentFrame, "Unknown", new Point(face.X - 2, face.Y - 2),
+                                    FontFace.HersheyComplex, 1.0, new Bgr(Color.Orange).MCvScalar);
+                                CvInvoke.Rectangle(currentFrame, face, new Bgr(Color.Red).MCvScalar, 2);
+                            }
+                        }
                     }
                 }
             }
@@ -82,9 +149,19 @@ namespace Version2
             {
                 Directory.CreateDirectory(path);
             }
-            detectedFace.Resize(200, 200, Inter.Cubic).Save(path + @"\" + txtPersonName.Text + ".jpg");
-            Thread.Sleep(1000);
-            txtPersonName.Text = "";
+            Task.Factory.StartNew(() => {
+                for (int i = 0; i < 10; i++)
+                {
+                    //resize the image then saving it
+                    detectedFace.Resize(200, 200, Inter.Cubic).Save(path + @"\" + txtPersonName.Text + "_" + DateTime.Now.ToString("dd-mm-yyyy-hh-mm-ss") + ".jpg");
+                    Thread.Sleep(1000);
+                    if (i + 1 == 10)
+                    {
+                        loadSavedFaceFiles();
+                        Thread.Sleep(5000);
+                    }
+                }
+            });
         }
 
         private void startCameraCapture(object sender, EventArgs e)
